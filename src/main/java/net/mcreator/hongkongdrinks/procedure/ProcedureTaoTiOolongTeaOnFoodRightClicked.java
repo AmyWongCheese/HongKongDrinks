179e4825dbfa2931c4376bb1cc413a144a51f2a9
mmwong <mmwong@MMWONG-PC.jccsstm.local>
2020-05-29 10:35:24 +0800

    Oolong Tea

--- src/main/java/net/mcreator/hongkongdrinks/procedure/ProcedureTaoTiOolongTeaOnFoodRightClicked.java
+++ src/main/java/net/mcreator/hongkongdrinks/procedure/ProcedureTaoTiOolongTeaOnFoodRightClicked.java
@@ -10,35 +10,35 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.Entity;
 
-import net.mcreator.hongkongdrinks.item.ItemTaoTiMandarinLemon;
-import net.mcreator.hongkongdrinks.entity.EntityTaoTiMandarinLemonEntity;
+import net.mcreator.hongkongdrinks.item.ItemTaoTiOolongTea;
+import net.mcreator.hongkongdrinks.entity.EntityTaoTiOolongTeaEntity;
 import net.mcreator.hongkongdrinks.ElementsHongKongDrinks;
 
 @ElementsHongKongDrinks.ModElement.Tag
-public class ProcedureTaoTiMandarinLemonOnFoodRightClicked extends ElementsHongKongDrinks.ModElement {
-	public ProcedureTaoTiMandarinLemonOnFoodRightClicked(ElementsHongKongDrinks instance) {
-		super(instance, 106);
+public class ProcedureTaoTiOolongTeaOnFoodRightClicked extends ElementsHongKongDrinks.ModElement {
+	public ProcedureTaoTiOolongTeaOnFoodRightClicked(ElementsHongKongDrinks instance) {
+		super(instance, 111);
 	}
 
 	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
-			System.err.println("Failed to load dependency entity for procedure TaoTiMandarinLemonOnFoodRightClicked!");
+			System.err.println("Failed to load dependency entity for procedure TaoTiOolongTeaOnFoodRightClicked!");
 			return;
 		}
 		if (dependencies.get("x") == null) {
-			System.err.println("Failed to load dependency x for procedure TaoTiMandarinLemonOnFoodRightClicked!");
+			System.err.println("Failed to load dependency x for procedure TaoTiOolongTeaOnFoodRightClicked!");
 			return;
 		}
 		if (dependencies.get("y") == null) {
-			System.err.println("Failed to load dependency y for procedure TaoTiMandarinLemonOnFoodRightClicked!");
+			System.err.println("Failed to load dependency y for procedure TaoTiOolongTeaOnFoodRightClicked!");
 			return;
 		}
 		if (dependencies.get("z") == null) {
-			System.err.println("Failed to load dependency z for procedure TaoTiMandarinLemonOnFoodRightClicked!");
+			System.err.println("Failed to load dependency z for procedure TaoTiOolongTeaOnFoodRightClicked!");
 			return;
 		}
 		if (dependencies.get("world") == null) {
-			System.err.println("Failed to load dependency world for procedure TaoTiMandarinLemonOnFoodRightClicked!");
+			System.err.println("Failed to load dependency world for procedure TaoTiOolongTeaOnFoodRightClicked!");
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
@@ -46,23 +46,22 @@ public class ProcedureTaoTiMandarinLemonOnFoodRightClicked extends ElementsHongK
 		float y = (float) entity.posY;
 		float z = (float) entity.posZ;
 		World world = (World) dependencies.get("world");
-		if ((((entity.isSneaking()) && (new ItemStack(ItemTaoTiMandarinLemon.block, (int) (1))
+		if ((((entity.isSneaking()) && (new ItemStack(ItemTaoTiOolongTea.block, (int) (1))
 				.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))
-				|| ((new ItemStack(ItemTaoTiMandarinLemon.block, (int) (1))
+				|| ((new ItemStack(ItemTaoTiOolongTea.block, (int) (1))
 						.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemOffhand() : ItemStack.EMPTY)
 								.getItem())
 						&& (new ItemStack(Blocks.AIR, (int) (1)).getItem() == ((entity instanceof EntityLivingBase)
 								? ((EntityLivingBase) entity).getHeldItemMainhand()
 								: ItemStack.EMPTY).getItem())))) {
 			if (entity instanceof EntityPlayer)
-				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemTaoTiMandarinLemon.block, (int) (1)).getItem(), -1, (int) 1,
-						null);
+				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemTaoTiOolongTea.block, (int) (1)).getItem(), -1, (int) 1, null);
 			world.playSound((EntityPlayer) null, x, y, z,
 					(net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.wood.place")),
 					SoundCategory.NEUTRAL, (float) 1, (float) 1);
 			if (((entity.getHorizontalFacing()) == EnumFacing.NORTH)) {
 				if (!world.isRemote) {
-					Entity entityToSpawn = new EntityTaoTiMandarinLemonEntity.EntityCustom(world);
+					Entity entityToSpawn = new EntityTaoTiOolongTeaEntity.EntityCustom(world);
 					if (entityToSpawn != null) {
 						entityToSpawn.setLocationAndAngles((x), (y + 1), (z - 1), world.rand.nextFloat() * 360F, 0.0F);
 						world.spawnEntity(entityToSpawn);
@@ -70,7 +69,7 @@ public class ProcedureTaoTiMandarinLemonOnFoodRightClicked extends ElementsHongK
 				}
 			} else if (((entity.getHorizontalFacing()) == EnumFacing.SOUTH)) {
 				if (!world.isRemote) {
-					Entity entityToSpawn = new EntityTaoTiMandarinLemonEntity.EntityCustom(world);
+					Entity entityToSpawn = new EntityTaoTiOolongTeaEntity.EntityCustom(world);
 					if (entityToSpawn != null) {
 						entityToSpawn.setLocationAndAngles((x), (y + 1), (z + 1), world.rand.nextFloat() * 360F, 0.0F);
 						world.spawnEntity(entityToSpawn);
@@ -78,7 +77,7 @@ public class ProcedureTaoTiMandarinLemonOnFoodRightClicked extends ElementsHongK
 				}
 			} else if (((entity.getHorizontalFacing()) == EnumFacing.EAST)) {
 				if (!world.isRemote) {
-					Entity entityToSpawn = new EntityTaoTiMandarinLemonEntity.EntityCustom(world);
+					Entity entityToSpawn = new EntityTaoTiOolongTeaEntity.EntityCustom(world);
 					if (entityToSpawn != null) {
 						entityToSpawn.setLocationAndAngles((x + 1), (y + 1), (z), world.rand.nextFloat() * 360F, 0.0F);
 						world.spawnEntity(entityToSpawn);
@@ -86,7 +85,7 @@ public class ProcedureTaoTiMandarinLemonOnFoodRightClicked extends ElementsHongK
 				}
 			} else if (((entity.getHorizontalFacing()) == EnumFacing.WEST)) {
 				if (!world.isRemote) {
-					Entity entityToSpawn = new EntityTaoTiMandarinLemonEntity.EntityCustom(world);
+					Entity entityToSpawn = new EntityTaoTiOolongTeaEntity.EntityCustom(world);
 					if (entityToSpawn != null) {
 						entityToSpawn.setLocationAndAngles((x - 1), (y + 1), (z), world.rand.nextFloat() * 360F, 0.0F);
 						world.spawnEntity(entityToSpawn);
