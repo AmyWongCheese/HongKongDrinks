8b8f50f175e2f64fcdfd983c4fc78a3044ce6bb9
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-09-08 12:23:28 +0800

    Lychee Love Tea

---
+++ src/main/java/com/neocosplayer/hongkongdrinks/entity/EntityVitaLycheeLoveTeaEntity.java
@@ -0,0 +1,205 @@
+
+package com.neocosplayer.hongkongdrinks.entity;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.DamageSource;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.model.ModelBase;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+import com.neocosplayer.hongkongdrinks.procedure.ProcedureVitaLycheeLoveTeaEntityEntityIsHurt;
+import com.neocosplayer.hongkongdrinks.item.ItemVitaLycheeLoveTea;
+import com.neocosplayer.hongkongdrinks.ElementsHongkongdrinksMod;
+
+@ElementsHongkongdrinksMod.ModElement.Tag
+public class EntityVitaLycheeLoveTeaEntity extends ElementsHongkongdrinksMod.ModElement {
+	public static final int ENTITYID = 95;
+	public static final int ENTITYID_RANGED = 96;
+	public EntityVitaLycheeLoveTeaEntity(ElementsHongkongdrinksMod instance) {
+		super(instance, 228);
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> EntityEntryBuilder.create().entity(EntityCustom.class)
+				.id(new ResourceLocation("hongkongdrinks", "vita_lychee_love_tea_entity"), ENTITYID).name("vita_lychee_love_tea_entity")
+				.tracker(100, 3, true).build());
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+		Biome[] spawnBiomes = allbiomes(Biome.REGISTRY);
+		EntityRegistry.addSpawn(EntityCustom.class, 1, 1, 1, EnumCreatureType.CREATURE, spawnBiomes);
+	}
+
+	private Biome[] allbiomes(net.minecraft.util.registry.RegistryNamespaced<ResourceLocation, Biome> in) {
+		Iterator<Biome> itr = in.iterator();
+		ArrayList<Biome> ls = new ArrayList<Biome>();
+		while (itr.hasNext())
+			ls.add(itr.next());
+		return ls.toArray(new Biome[ls.size()]);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		RenderingRegistry.registerEntityRenderingHandler(EntityCustom.class, renderManager -> {
+			return new RenderLiving(renderManager, new Modelvitabottle(), 0.05f) {
+				protected ResourceLocation getEntityTexture(Entity entity) {
+					return new ResourceLocation("hongkongdrinks:textures/vitalycheelovetea_e.png");
+				}
+			};
+		});
+	}
+	public static class EntityCustom extends EntityCreature {
+		public EntityCustom(World world) {
+			super(world);
+			setSize(0.2f, 0.5f);
+			experienceValue = 0;
+			this.isImmuneToFire = true;
+			setNoAI(!true);
+			enablePersistence();
+		}
+
+		@Override
+		protected void initEntityAI() {
+			super.initEntityAI();
+			this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityPlayer.class, (float) 3));
+		}
+
+		@Override
+		public EnumCreatureAttribute getCreatureAttribute() {
+			return EnumCreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		protected boolean canDespawn() {
+			return false;
+		}
+
+		@Override
+		protected Item getDropItem() {
+			return new ItemStack(ItemVitaLycheeLoveTea.block, (int) (1)).getItem();
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getAmbientSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation(""));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation(""));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation(""));
+		}
+
+		@Override
+		protected float getSoundVolume() {
+			return 1.0F;
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			int x = (int) this.posX;
+			int y = (int) this.posY;
+			int z = (int) this.posZ;
+			Entity entity = this;
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				ProcedureVitaLycheeLoveTeaEntityEntityIsHurt.executeProcedure($_dependencies);
+			}
+			if (source.getImmediateSource() instanceof EntityArrow)
+				return false;
+			if (source.getImmediateSource() instanceof EntityPotion)
+				return false;
+			if (source == DamageSource.FALL)
+				return false;
+			if (source == DamageSource.CACTUS)
+				return false;
+			if (source == DamageSource.DROWN)
+				return false;
+			if (source == DamageSource.LIGHTNING_BOLT)
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		@Override
+		protected void applyEntityAttributes() {
+			super.applyEntityAttributes();
+			if (this.getEntityAttribute(SharedMonsterAttributes.ARMOR) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(0D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(10D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(100D);
+			if (this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
+				this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(0D);
+		}
+	}
+
+	// Made with Blockbench 3.6.6
+	// Exported for Minecraft version 1.12
+	// Paste this class into your mod and generate all required imports
+	public static class Modelvitabottle extends ModelBase {
+		private final ModelRenderer bone;
+		public Modelvitabottle() {
+			textureWidth = 20;
+			textureHeight = 20;
+			bone = new ModelRenderer(this);
+			bone.setRotationPoint(0.0F, 24.0F, 0.0F);
+			bone.cubeList.add(new ModelBox(bone, 12, 0, -1.0F, -12.0F, -1.0F, 2, 2, 2, 0.0F, false));
+			bone.cubeList.add(new ModelBox(bone, 0, 0, -2.0F, -10.0F, -2.0F, 4, 3, 4, 0.0F, false));
+			bone.cubeList.add(new ModelBox(bone, 0, 11, -1.75F, -7.0F, -1.75F, 3, 4, 3, 0.0F, false));
+			bone.cubeList.add(new ModelBox(bone, 0, 7, -2.0F, -3.0F, -2.0F, 4, 3, 4, 0.0F, false));
+		}
+
+		@Override
+		public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+			bone.render(f5);
+		}
+
+		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+			modelRenderer.rotateAngleX = x;
+			modelRenderer.rotateAngleY = y;
+			modelRenderer.rotateAngleZ = z;
+		}
+
+		public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
+			super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
+		}
+	}
+}
---
+++ src/main/java/com/neocosplayer/hongkongdrinks/procedure/ProcedureVitaLycheeLoveTeaRightClickedInAir.java
@@ -0,0 +1,49 @@
+package com.neocosplayer.hongkongdrinks.procedure;
+
+import net.minecraft.world.World;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import com.neocosplayer.hongkongdrinks.item.ItemVitaLycheeLoveTea;
+import com.neocosplayer.hongkongdrinks.entity.EntityVitaLycheeLoveTeaEntity;
+import com.neocosplayer.hongkongdrinks.ElementsHongkongdrinksMod;
+
+@ElementsHongkongdrinksMod.ModElement.Tag
+public class ProcedureVitaLycheeLoveTeaRightClickedInAir extends ElementsHongkongdrinksMod.ModElement {
+	public ProcedureVitaLycheeLoveTeaRightClickedInAir(ElementsHongkongdrinksMod instance) {
+		super(instance, 229);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		Entity entityToSpawn = new EntityVitaLycheeLoveTeaEntity.EntityCustom(world);
+
+		if ((((entity.isSneaking()) && (new ItemStack(ItemVitaLycheeLoveTea.block, (int) (1))
+				.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))
+				|| ((new ItemStack(ItemVitaLycheeLoveTea.block, (int) (1))
+						.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+								.getItem())
+						&& (new ItemStack(Blocks.AIR, (int) (1)).getItem() == ((entity instanceof EntityLivingBase)
+								? ((EntityLivingBase) entity).getHeldItemMainhand()
+								: ItemStack.EMPTY).getItem())))) {
+			if (entity instanceof EntityPlayer)
+				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemVitaLycheeLoveTea.block, (int) (1)).getItem(), -1, (int) 1,
+						null);
+			world.playSound((EntityPlayer) null, x, y, z,
+					(net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.wood.place")),
+					SoundCategory.NEUTRAL, (float) 1, (float) 1);
+
+			DrinkPlaced.place(entity, world, entityToSpawn);		
+
+		}
+	}
+}
