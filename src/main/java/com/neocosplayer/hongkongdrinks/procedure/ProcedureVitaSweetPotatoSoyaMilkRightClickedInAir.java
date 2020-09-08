1f499289e46c058589b257e130b485e03d705628
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-09-08 13:21:30 +0800

    Sweet Potato

---
+++ src/main/java/com/neocosplayer/hongkongdrinks/procedure/ProcedureVitaSweetPotatoSoyaMilkRightClickedInAir.java
@@ -0,0 +1,48 @@
+package com.neocosplayer.hongkongdrinks.procedure;
+
+import net.minecraft.world.World;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import com.neocosplayer.hongkongdrinks.item.ItemVitaSweetPotatoSoyaMilk;
+import com.neocosplayer.hongkongdrinks.entity.EntityVitaSweetPotatoSoyaMilkEntity;
+import com.neocosplayer.hongkongdrinks.ElementsHongkongdrinksMod;
+
+@ElementsHongkongdrinksMod.ModElement.Tag
+public class ProcedureVitaSweetPotatoSoyaMilkRightClickedInAir extends ElementsHongkongdrinksMod.ModElement {
+	public ProcedureVitaSweetPotatoSoyaMilkRightClickedInAir(ElementsHongkongdrinksMod instance) {
+		super(instance, 234);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		Entity entityToSpawn = new EntityVitaSweetPotatoSoyaMilkEntity.EntityCustom(world);
+		
+		if ((((entity.isSneaking()) && (new ItemStack(ItemVitaSweetPotatoSoyaMilk.block, (int) (1))
+				.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))
+				|| ((new ItemStack(ItemVitaSweetPotatoSoyaMilk.block, (int) (1))
+						.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+								.getItem())
+						&& (new ItemStack(Blocks.AIR, (int) (1)).getItem() == ((entity instanceof EntityLivingBase)
+								? ((EntityLivingBase) entity).getHeldItemMainhand()
+								: ItemStack.EMPTY).getItem())))) {
+			if (entity instanceof EntityPlayer)
+				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemVitaSweetPotatoSoyaMilk.block, (int) (1)).getItem(), -1,
+						(int) 1, null);
+			world.playSound((EntityPlayer) null, x, y, z,
+					(net.minecraft.util.SoundEvent) net.minecraft.util.SoundEvent.REGISTRY.getObject(new ResourceLocation("block.wood.place")),
+					SoundCategory.NEUTRAL, (float) 1, (float) 1);
+
+			DrinkPlaced.place(entity, world, entityToSpawn);
+		}
+	}
+}
