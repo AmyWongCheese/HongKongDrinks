0e8cceb19716d172476fa4d97e5e306fad23cf91
AmyWongCheese <mmwong@MMWONG-PC.jccsstm.local>
2020-09-10 15:02:09 +0800

    Tiramisu

---
+++ src/main/java/com/neocosplayer/hongkongdrinks/procedure/ProcedureVitaTiramisuDrinkBoyRightClickedInAir.java
@@ -0,0 +1,44 @@
+package com.neocosplayer.hongkongdrinks.procedure;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import com.neocosplayer.hongkongdrinks.item.ItemVitaTiramisuDrinkBoy;
+import com.neocosplayer.hongkongdrinks.entity.EntityVitaTiramisuDrinkBoyEntity;
+import com.neocosplayer.hongkongdrinks.ElementsHongkongdrinksMod;
+
+@ElementsHongkongdrinksMod.ModElement.Tag
+public class ProcedureVitaTiramisuDrinkBoyRightClickedInAir extends ElementsHongkongdrinksMod.ModElement {
+	public ProcedureVitaTiramisuDrinkBoyRightClickedInAir(ElementsHongkongdrinksMod instance) {
+		super(instance, 270);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		Entity entityToSpawn = new EntityVitaTiramisuDrinkBoyEntity.EntityCustom(world);
+		
+		if ((((entity.isSneaking()) && (new ItemStack(ItemVitaTiramisuDrinkBoy.block, (int) (1))
+				.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))
+				|| ((new ItemStack(ItemVitaTiramisuDrinkBoy.block, (int) (1))
+						.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+								.getItem())
+						&& (new ItemStack(Blocks.AIR, (int) (1)).getItem() == ((entity instanceof EntityLivingBase)
+								? ((EntityLivingBase) entity).getHeldItemMainhand()
+								: ItemStack.EMPTY).getItem())))) {
+			if (entity instanceof EntityPlayer)
+				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemVitaTiramisuDrinkBoy.block, (int) (1)).getItem(), -1, (int) 1,
+						null);
+
+			DrinkPlaced.place(entity, world, entityToSpawn);
+
+		}
+	}
+}
---
+++ src/main/java/com/neocosplayer/hongkongdrinks/procedure/ProcedureVitaTiramisuDrinkGirlRightClickedInAir.java
@@ -0,0 +1,44 @@
+package com.neocosplayer.hongkongdrinks.procedure;
+
+import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
+import net.minecraft.init.Blocks;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.Entity;
+
+import com.neocosplayer.hongkongdrinks.item.ItemVitaTiramisuDrinkGirl;
+import com.neocosplayer.hongkongdrinks.entity.EntityVitaTiramisuDrinkGirlEntity;
+import com.neocosplayer.hongkongdrinks.ElementsHongkongdrinksMod;
+
+@ElementsHongkongdrinksMod.ModElement.Tag
+public class ProcedureVitaTiramisuDrinkGirlRightClickedInAir extends ElementsHongkongdrinksMod.ModElement {
+	public ProcedureVitaTiramisuDrinkGirlRightClickedInAir(ElementsHongkongdrinksMod instance) {
+		super(instance, 275);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		Entity entity = (Entity) dependencies.get("entity");
+		int x = (int) dependencies.get("x");
+		int y = (int) dependencies.get("y");
+		int z = (int) dependencies.get("z");
+		World world = (World) dependencies.get("world");
+		Entity entityToSpawn = new EntityVitaTiramisuDrinkGirlEntity.EntityCustom(world);
+		
+		if ((((entity.isSneaking()) && (new ItemStack(ItemVitaTiramisuDrinkGirl.block, (int) (1))
+				.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemMainhand() : ItemStack.EMPTY).getItem()))
+				|| ((new ItemStack(ItemVitaTiramisuDrinkGirl.block, (int) (1))
+						.getItem() == ((entity instanceof EntityLivingBase) ? ((EntityLivingBase) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+								.getItem())
+						&& (new ItemStack(Blocks.AIR, (int) (1)).getItem() == ((entity instanceof EntityLivingBase)
+								? ((EntityLivingBase) entity).getHeldItemMainhand()
+								: ItemStack.EMPTY).getItem())))) {
+			if (entity instanceof EntityPlayer)
+				((EntityPlayer) entity).inventory.clearMatchingItems(new ItemStack(ItemVitaTiramisuDrinkGirl.block, (int) (1)).getItem(), -1, (int) 1,
+						null);
+
+			DrinkPlaced.place(entity, world, entityToSpawn);
+
+		}
+	}
+}
